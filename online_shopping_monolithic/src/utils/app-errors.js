b0e469d5eb762c0d3bb06ec01582e072260a5a78
Tserendorj Tumen <tserendortj@Tserendorjs-MacBook-Pro.local>
2023-12-05 05:42:11 +0800

    initial commit

---
+++ online_shopping_monolithic/src/utils/app-errors.js
@@ -0,0 +1,70 @@
+const STATUS_CODE = {
+    OK: 200,
+    BAD_REQUEST: 400,
+    UN_AUTHORISED: 403,
+    NOT_FOUND: 404,
+    INTERNAL_ERROR: 500,
+};
+
+class AppError extends Error {
+    constructor(
+        name,
+        statusCode,
+        description,
+        isOperational,
+        errorStack,
+        loginErrorResponse
+    ){
+        super(description);
+        Object.setPrototypeOf(this, new.target.prototype);
+        this.name = name;
+        this.isOperational = isOperational;
+        this.errorStack = errorStack;
+        this.logError = loginErrorResponse;
+        Error.captureStackTrace(this);
+    }       
+}
+
+class APIError extends AppError {
+    constructor(
+        name, 
+        statusCode = STATUS_CODE.INTERNAL_ERROR,
+        description = "Internal Server Error",
+        isOperational = true
+        ){
+            super(name, statusCode, description, isOperational);
+        }
+}
+
+class BadRequestError extends AppError {
+    constructor(description = "Bad request", loginErrorResponse){
+        super(
+            "NOT FOUND",
+            STATUS_CODES.BAD_REQUEST,
+            description,
+            true,
+            false,
+            loginErrorResponse
+        );
+    }
+}
+
+class ValidationError extends AppError {
+    constructor(description = 'Validation Error', errorStack){
+        super(
+            "BAD REQUEST",
+            STATUS_CODES.BAD_REQUEST,
+            description,
+            true,
+            errorStack
+        )
+    }
+}
+
+module.exports = {
+    AppError,
+    APIError,
+    BadRequestError,
+    ValidationError,
+    STATUS_CODES,
+}
