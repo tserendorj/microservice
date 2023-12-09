dbc141ab297a57962fc85aa6bd7dba2e790ceb7b
Tserendorj Tumen <tserendortj@Tserendorjs-MacBook-Pro.local>
2023-12-09 08:47:07 +0800

    services done

---
+++ online_shopping_monolithic/src/services/shopping-service.js
@@ -0,0 +1,30 @@
+const {ShoppingRepository} = require('../database')
+const {FormateData} = require('../utils');
+const { APIError } = require('../utils/app-errors');
+
+class ShoppingService{
+    constructor(){
+        this.repository = new ShoppingRepository();
+    }
+    async PlaceOrder(userInput){
+        const {_id, txnNumber} = userInput;
+
+        try {
+            const orderResult = await this.repository.CreateNewOrder(_id, txnNumber);
+            return FormateData(orderResult);
+        } catch(err){
+            throw new APIError('Data Not found', err);
+        }
+    }
+
+    async GetOrders(customerId){
+        try {
+            const orders = await this.repository.Orders(customerId);
+            return FormateData(orders);
+        } catch(err){
+            throw new APIError('Data Not Found', err);
+        }
+    }
+}
+
+module.exports = ShoppingService;
